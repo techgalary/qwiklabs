0b662a3fe0fcb3242d09e533fb641ef5fda14554
sowmya <techgalari@gmail.com>
2024-11-21 16:56:12 +0530

    added new file

--- gcp-challenge-labs/Create_and_Manage_Bigtable_Instances_Challenge_Lab/script.sh
+++ gcp-challenge-labs/Create_and_Manage_Bigtable_Instances_Challenge_Lab/script.sh
@@ -7,7 +7,7 @@ CLUSTER_ID_C1="ecommerce-recommendations-c1"
 CLUSTER_ID_C2="ecommerce-recommendations-c2"
 REGION="us-west1"
 ZONE_C1="us-west1-b"
-ZONE_C2="us-west1-c" # Change to any available zone if needed
+ZONE_C2="us-west1-c" # Change if necessary
 TABLE_SESSION="SessionHistory"
 TABLE_PERSONALIZED="PersonalizedProducts"
 RESTORED_TABLE="PersonalizedProducts_7_restored"
@@ -22,47 +22,47 @@ gcloud bigtable instances create $INSTANCE_ID \
   --cluster-zone=$ZONE_C1 \
   --display-name="Ecommerce Recommendations" \
   --instance-type=PRODUCTION \
-  --autoscaling-min-nodes=1 \
-  --autoscaling-max-nodes=5 \
-  --autoscaling-cpu-target=60 \
-  --storage-type=SSD
+  --cluster-storage-type=SSD
 
 echo "Task 2: Creating and populating Bigtable tables..."
-# Create SessionHistory table
-gcloud bigtable tables create $TABLE_SESSION --instance=$INSTANCE_ID
+# Create SessionHistory table with column families
+gcloud bigtable tables create $TABLE_SESSION --instance=$INSTANCE_ID \
+  --column-families=Engagements,Sales
 
 # Load data into SessionHistory table
 gcloud dataflow jobs run import-sessions \
-  --gcs-location=gs://dataflow-templates/latest/SequenceFile-to-Cloud-Bigtable \
+  --gcs-location=gs://dataflow-templates-us/latest/SequenceFile-to-Bigtable \
   --region=$REGION \
   --staging-location=gs://$PROJECT_ID/temp/ \
   --parameters=\
-bigtableProject=$PROJECT_ID,\
+bigtableProjectId=$PROJECT_ID,\
 bigtableInstanceId=$INSTANCE_ID,\
 bigtableTableId=$TABLE_SESSION,\
 inputFilePattern=$SESSIONS_FILE
 
-# Create PersonalizedProducts table
-gcloud bigtable tables create $TABLE_PERSONALIZED --instance=$INSTANCE_ID
+# Create PersonalizedProducts table with column families
+gcloud bigtable tables create $TABLE_PERSONALIZED --instance=$INSTANCE_ID \
+  --column-families=Recommendations
 
 # Load data into PersonalizedProducts table
 gcloud dataflow jobs run import-recommendations \
-  --gcs-location=gs://dataflow-templates/latest/SequenceFile-to-Cloud-Bigtable \
+  --gcs-location=gs://dataflow-templates-us/latest/SequenceFile-to-Bigtable \
   --region=$REGION \
   --staging-location=gs://$PROJECT_ID/temp/ \
   --parameters=\
-bigtableProject=$PROJECT_ID,\
+bigtableProjectId=$PROJECT_ID,\
 bigtableInstanceId=$INSTANCE_ID,\
 bigtableTableId=$TABLE_PERSONALIZED,\
 inputFilePattern=$RECOMMENDATIONS_FILE
 
 echo "Task 3: Configuring replication for Bigtable..."
-gcloud bigtable instances add-cluster $INSTANCE_ID \
-  --cluster=$CLUSTER_ID_C2 \
-  --cluster-zone=$ZONE_C2 \
+gcloud bigtable clusters create $CLUSTER_ID_C2 \
+  --instance=$INSTANCE_ID \
+  --zone=$ZONE_C2 \
   --autoscaling-min-nodes=1 \
   --autoscaling-max-nodes=5 \
-  --autoscaling-cpu-target=60
+  --autoscaling-cpu-target=60 \
+  --storage-type=SSD
 
 echo "Task 4: Backing up and restoring data..."
 # Create a backup for PersonalizedProducts table
@@ -70,13 +70,13 @@ gcloud bigtable backups create $BACKUP_ID \
   --instance=$INSTANCE_ID \
   --cluster=$CLUSTER_ID_C1 \
   --table=$TABLE_PERSONALIZED \
-  --expire-time=$(date -u -d "+7 days" '+%Y-%m-%dT%H:%M:%SZ')
+  --expiration-date=$(date -u -d "+7 days" '+%Y-%m-%dT%H:%M:%SZ')
 
 # Restore the backup to a new table
 gcloud bigtable backups restore $BACKUP_ID \
   --instance=$INSTANCE_ID \
   --cluster=$CLUSTER_ID_C1 \
-  --table=$RESTORED_TABLE
+  --destination-table=$RESTORED_TABLE
 
 echo "Task 5: Deleting Bigtable data and instance..."
 # Delete tables
